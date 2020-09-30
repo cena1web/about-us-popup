9481e5a9a4ab3daa989faa436b2b22f0320918bc
cena1web <60264674+cena1web@users.noreply.github.com>
2020-09-30 16:04:36 +0300

    Create aboutus-popup.js

---
+++ aboutus-popup.js
@@ -0,0 +1,4 @@
+// Total Posts by : www.cena1web.com
+function getposts(json){var totalposts=json.feed.openSearch$totalResults.$t;document.write('<div class="totalposts">
+<span class="totalnumber">'+totalposts+'</span><span class="totallabel">موضوع منشور على الموقع</span></div>
+');}
